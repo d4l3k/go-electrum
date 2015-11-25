a77e4df6a7af9144f245d2c4d133526492996fd2
Tristan Rice <rice@fn.lc>
2015-11-24 22:19:47 -0800

    Implemented the rest of the documented functions

--- blockchain.go
+++ blockchain.go
@@ -12,11 +12,11 @@ func (n *Node) BlockchainNumBlocksSubscribe() (int, error) {
 
 type BlockchainHeader struct {
 	Nonce         uint64 `json:"nonce"`
-	PrevBlockHash []byte `json:"prev_block_hash"`
+	PrevBlockHash string `json:"prev_block_hash"`
 	Timestamp     uint64 `json:"timestamp"`
-	MerkleRoot    []byte `json:"merkle_root"`
+	MerkleRoot    string `json:"merkle_root"`
 	BlockHeight   uint64 `json:"block_height"`
-	UtxoRoot      []byte `json:"utxo_root"`
+	UtxoRoot      string `json:"utxo_root"`
 	Version       int    `json:"version"`
 	Bits          uint64 `json:"bits"`
 }
@@ -33,29 +33,66 @@ func (n *Node) BlockchainHeadersSubscribe() (*BlockchainHeader, error) {
 	return resp.Result, err
 }
 
-// TODO(d4l3k) implement
+// BlockchainAddressSubscribe subscribes to transactions on an address and
+// returns the hash of the transaction history.
+// TODO (d4l3k) handle address updates
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-subscribe
-func (n *Node) BlockchainAddressSubscribe() error { return ErrNotImplemented }
+func (n *Node) BlockchainAddressSubscribe(address string) (string, error) {
+	resp := &basicResp{}
+	err := n.request("blockchain.address.subscribe", []string{address}, resp)
+	return resp.Result, err
+}
 
-// TODO(d4l3k) implement
+type Transaction struct {
+	Hash   string `json:"tx_hash"`
+	Height int    `json:"height"`
+	Value  int    `json:"value"`
+	Pos    int    `json:"tx_pos"`
+}
+
+// BlockchainAddressGetHistory returns the history of an address.
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-get-history
-func (n *Node) BlockchainAddressGetHistory() error { return ErrNotImplemented }
+func (n *Node) BlockchainAddressGetHistory(address string) ([]*Transaction, error) {
+	resp := &struct {
+		Result []*Transaction `json:"result"`
+	}{}
+	err := n.request("blockchain.address.get_history", []string{address}, resp)
+	return resp.Result, err
+}
 
 // TODO(d4l3k) implement
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-get-mempool
 func (n *Node) BlockchainAddressGetMempool() error { return ErrNotImplemented }
 
-// TODO(d4l3k) implement
+type Balance struct {
+	Confirmed   int `json:"confirmed"`
+	Unconfirmed int `json:"unconfirmed"`
+}
+
+// BlockchainAddressGetBalance returns the balance of an address.
+// TODO (d4l3k) investigate `error from server: "'Node' object has no attribute '__getitem__'"`
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-get-balance
-func (n *Node) BlockchainAddressGetBalance() error { return ErrNotImplemented }
+func (n *Node) BlockchainAddressGetBalance(address string) (*Balance, error) {
+	resp := &struct {
+		Result *Balance `json:"result"`
+	}{}
+	err := n.request("blockchain.address.get_balance", []string{address}, resp)
+	return resp.Result, err
+}
 
 // TODO(d4l3k) implement
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-get-proof
 func (n *Node) BlockchainAddressGetProof() error { return ErrNotImplemented }
 
-// TODO(d4l3k) implement
+// BlockchainAddressListUnspent lists the unspent transactions for the given address.
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-address-listunspent
-func (n *Node) BlockchainAddressListunspent() error { return ErrNotImplemented }
+func (n *Node) BlockchainAddressListUnspent(address string) ([]*Transaction, error) {
+	resp := &struct {
+		Result []*Transaction `json:"result"`
+	}{}
+	err := n.request("blockchain.address.listunspent", []string{address}, resp)
+	return resp.Result, err
+}
 
 // TODO(d4l3k) implement
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-utxo-get-address
@@ -77,6 +114,10 @@ func (n *Node) BlockchainTransactionBroadcast() error { return ErrNotImplemented
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-transaction-get-merkle
 func (n *Node) BlockchainTransactionGetMerkle() error { return ErrNotImplemented }
 
-// TODO(d4l3k) implement
+// BlockchainTransactionGet returns the raw transaction (hex-encoded) for the given txid. If transaction doesn't exist, an error is returned.
 // http://docs.electrum.org/en/latest/protocol.html#blockchain-transaction-get
-func (n *Node) BlockchainTransactionGet() error { return ErrNotImplemented }
+func (n *Node) BlockchainTransactionGet(txid string) (string, error) {
+	resp := &basicResp{}
+	err := n.request("blockchain.transaction.get", []string{txid}, resp)
+	return resp.Result, err
+}
--- example/singlenode.go
+++ example/singlenode.go
@@ -11,34 +11,71 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	version, err := node.ServerVersion()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Version: %s", version)
+
 	banner, err := node.ServerBanner()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Banner: %s", banner)
+
 	address, err := node.ServerDonationAddress()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Address: %s", address)
+
 	peers, err := node.ServerPeersSubscribe()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Peers: %+v", peers)
+
 	numblocks, err := node.BlockchainNumBlocksSubscribe()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Numblocks: %+v", numblocks)
+
 	header, err := node.BlockchainHeadersSubscribe()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Headers: %+v", header)
+
+	hash, err := node.BlockchainAddressSubscribe("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Address history hash: %+v", hash)
+
+	history, err := node.BlockchainAddressGetHistory("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Address history: %+v", history)
+
+	transaction, err := node.BlockchainTransactionGet("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Transaction: %s", transaction)
+
+	transactions, err := node.BlockchainAddressListUnspent("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Unspent transactions: %+v", transactions)
+
+	// TODO(d4l3k) seems to not work, need to subscribe first maybe?
+	balance, err := node.BlockchainAddressGetBalance("1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Address balance: %+v", balance)
 }
